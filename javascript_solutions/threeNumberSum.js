bba54c4b38d6c2e37aebfe6ca095aac685f5cc4b
melo616 <jarboek1@gmail.com>
2024-01-11 12:45:13 -0600

    Edited and added comment

--- javascript_solutions/threeNumberSum.js
+++ javascript_solutions/threeNumberSum.js
@@ -1,13 +1,12 @@
+//This solution needs work because it doesn't pass all test cases. This is my first solution I was able to come up with for this problem.
 var threeSum = function(nums) {
     let sorted = nums.sort((a, b) => a - b);
     let result = [];
     for (let j = 1; j < sorted.length-1; j++) {
-        i = j-1;
+        let i = j-1;
         for (let k = j+1; k < sorted.length; k++) {
             let triplet = [];
-            if (sorted[i] + sorted[j] + sorted[k] > 0) {
-                break;
-            } else if (sorted[i] + sorted[j] + sorted[k] === 0) {
+            if (sorted[i] + sorted[j] + sorted[k] === 0) {
                 triplet.push(sorted[i], sorted[j], sorted[k]);
                 result.push(triplet);
             }
