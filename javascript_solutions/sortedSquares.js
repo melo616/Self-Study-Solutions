498f094b117bf6fc7b659aab8eb358a09535d09b
melo616 <jarboek1@gmail.com>
2024-01-14 10:25:34 -0600

    Added comments

--- javascript_solutions/sortedSquares.js
+++ javascript_solutions/sortedSquares.js
@@ -1,3 +1,5 @@
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
 var sortedSquares = function(nums) {
 
     let left = 0;
