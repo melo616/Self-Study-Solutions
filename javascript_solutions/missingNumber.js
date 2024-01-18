caaa883f21e56da60eafd522d129e00777e013e5
melo616 <jarboek1@gmail.com>
2024-01-18 11:37:54 -0600

    Added missing number hash solution

--- javascript_solutions/missingNumber.js
+++ javascript_solutions/missingNumber.js
@@ -2,15 +2,29 @@
 
 //Trivial solution: 
 //Time: O(n log n) - due to sorting function; Space: O(1)
+// function missingNumber(nums) {
+//     nums.sort((a, b) => a - b);
+//     for (i = 0; i < nums.length; i++) {
+//         if (nums[i] !== i) {
+//             return i;
+//         }
+//     }
+//     return i;
+// }
+
+// Hash solution 1: Time complexity O(n)
 function missingNumber(nums) {
-    nums.sort((a, b) => a - b);
-    for (i = 0; i < nums.length; i++) {
-        if (nums[i] !== i) {
-            return i;
+    let hash = {}
+    for (let i = 0; i < nums.length; i++) {
+        hash[nums[i]] = true;
+    }
+    console.log(hash)
+    for (let j = 0; j <= nums.length; j++) {
+        if (!hash[j]) {
+            return j;
         }
     }
-    return i;
-}
+};
 
 nums1 = [3,0,1] // 2
 console.log(missingNumber(nums1));
